b491621965425877b173cbc3c7f0fe2465596097
justintischendorf <154767599+justintischendorf@users.noreply.github.com>
2024-01-07 21:57:10 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'flex';
+}
+
+function closeSidebar(){
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'none';
+}
+
+function showDropdown(){
+    const dropdown = document.querySelector('.dropdown');
+    dropdown.style.display = 'flex';
+}
+
+function showDropdown2(){
+    const dropdown2 = document.querySelector('.dropdown2');
+    dropdown2.style.display = 'flex';
+}
